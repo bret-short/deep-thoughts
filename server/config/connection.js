cc0f5238cb9511efe6b166351d2a6a1f6d23fb1f
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2021-04-11 23:46:38 -0600

    updating the mongoose connector

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://bret-short:y95vDkwPepLVz6i@cluster0.wg8re.mongodb.net/deep_thoughts?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
